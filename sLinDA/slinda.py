0992efa981a245b07f33d27f2cb40dcdc6cd723f
Roman Martin <roman.martin@hhu.de>
2024-02-28 19:35:58 +0100

    Seprate function mechanism

--- sLinDA/slinda.py
+++ sLinDA/slinda.py
@@ -12,7 +12,7 @@ class sLinDAWrapper:
 
         if len(self.args.test):
             if self.args.test == 'server':
-                sLinDAserver(self.args.verbose, self.args.host)
+                sLinDAserver(self.args.verbose, self.args.host, len(self.args.p))
             elif self.args.test == 'client':
                 sLinDAclient(self.args.verbose, self.args.p[0])
 
@@ -23,8 +23,71 @@ class sLinDA:
 
 class sLinDAserver(sLinDA):
 
-    def __init__(self, verbose: int, host: str):
+    def __init__(self, verbose: int, host: str, clients: int = 0):
         super().__init__(verbose)
+
+        if clients == 0:
+            print("No clients awaiting, terminating server")
+            exit(100)
+        self.__await_responses(host, clients)
+        #self.__test(host)
+
+    def __await_responses(self, host: str, clients: int):
+        answers: dict = {}
+        host, port = host.split(":")
+
+        waiting = True
+        func = self.__first_contact
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((host, int(port)))
+            s.listen()
+
+            try:
+                while True:
+
+                    if waiting:
+                        waiting, response = self.__basic_inner_loop(s, func)
+                    else:
+                        break
+            except KeyboardInterrupt as e:
+                s.close()
+                print("Server: Closed server by manual interruption.")
+            except Exception as e:
+                print(e)
+
+    def __basic_inner_loop(self, s: socket.socket, func):
+        conn, addr = s.accept()
+        with conn:
+            if self.verbose >= 1:
+                print("Server: Client connected by %s" % str(addr))
+            while True:
+
+                if not func(conn):
+                    break
+
+                """data = conn.recv(1024)
+                if not data:
+                    break
+
+                if self.verbose >= 1:
+                    print("Server: Data received %s" % str(data.decode("utf8")))
+                msg: bytes = bytes('Thank you for connecting, I received %s' % data, encoding='utf8')
+                conn.sendall(msg)"""
+        return True, None
+
+    def __first_contact(self, conn):
+        data = conn.recv(1024)
+        if not data:
+            return False
+
+        if self.verbose >= 1:
+            print("Server: Data received %s" % str(data.decode("utf8")))
+        msg: bytes = bytes('Thank you for connecting, I received %s' % data, encoding='utf8')
+        conn.sendall(msg)
+        return True
+
+    def __test(self, host):
         host, port = host.split(":")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((host, int(port)))
@@ -55,15 +118,17 @@ class sLinDAclient(sLinDA):
 
     def __init__(self, verbose: int, peer: str):
         super().__init__(verbose)
+        self.__test(peer)
+
+    def __test(self, peer: str):
         host, port = peer.split(":")
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
             try:
                 s.connect((host, int(port)))
                 s.sendall(b"Hello, world")
                 data = s.recv(1024)
                 if self.verbose >= 1:
-                    print(f"Client: received data: {data!r}")
+                    print("Client: received data: %s" % str(data.decode('utf8')))
                 s.close()
             except ConnectionRefusedError as e:
                 print("Error: Connection refused by the peer")
@@ -82,6 +147,22 @@ def main():
 
     args = parser.parse_args()
     sLinDAWrapper(args)
+
+# aes
+"""
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+data = b'secret data'
+
+key = get_random_bytes(16)
+cipher = AES.new(key, AES.MODE_EAX)
+ciphertext, tag = cipher.encrypt_and_digest(data)
+nonce = cipher.nonce
+
+cipher = AES.new(key, AES.MODE_EAX, nonce)
+data = cipher.decrypt_and_verify(ciphertext, tag)
+"""
 
 if __name__ == "__main__":
     main()
